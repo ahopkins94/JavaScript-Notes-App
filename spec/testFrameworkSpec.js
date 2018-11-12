933e5db6b4b84fe12e8cc0fe183450a30954ed08
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-12 15:40:27 +0000

    working expect/toBe functions

--- spec/testFrameworkSpec.js
+++ spec/testFrameworkSpec.js
@@ -1,21 +1,47 @@
-var tester = require("../lib/tester.js")
+var Tester = require("../lib/tester.js");
 
-describe('describe', function(){
-  it('prints string to the console', function(){
-    spyOn(console, 'log');
-    tester.Describe('test', function(){
-      return ""
+describe('Tester', function() {
+  describe('describe', function(){
+    it('prints string to the console', function(){
+      spyOn(console, 'log');
+      Tester.describe('test', function(){
+        return "";
+      });
+      expect(console.log).toHaveBeenCalledWith('test');
     });
-    expect(console.log).toHaveBeenCalledWith('test');
+    it('executes callback function', function(){
+      var object = {
+        method: function(){
+          return "";
+        }
+      };
+      spyOn(object, 'method');
+      Tester.describe('test', object.method);
+      expect(object.method).toHaveBeenCalled();
+    });
   });
-  it('executes callback funciton', function(){
-    var object = {
-      method: function(){
-        return ""
-      }
-    }
-    spyOn(object, 'method');
-    tester.Describe('test', object.method)
-    expect(object.method).toHaveBeenCalled()
+  describe("expect", function() {
+    it('returns an object', function() {
+      var result = Tester.expect("argument");
+      expect(typeof result).toBe("object");
+    });
+    it('returns object with a "toBe" method', function() {
+      var result = Tester.expect("argument");
+      expect(typeof result.toBe).toBe("function");
+    });
+  });
+  describe("toBe", function() {
+    it('checks equality of its argument vs argument passed to "expect()"', function() {
+      spyOn(console, 'log');
+      var assertion = Tester.expect("hello");
+      expect(assertion.toBe("hello")).toEqual(true);
+      expect(console.log).toHaveBeenCalledWith('  :)');
+    });
+    it('checks inequality of its argument vs argument passed to "expect()"', function() {
+      spyOn(console, 'log');
+      var assertion = Tester.expect("hello");
+      expect(assertion.toBe("goodbye")).toEqual(false);
+      expect(console.log).toHaveBeenCalledWith('  Error: expected "hello" to equal "goodbye"');
+    });
   });
 });

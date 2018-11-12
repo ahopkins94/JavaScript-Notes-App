e0184705697fa42ef1a24237c12becf6eaec58c8
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-12 17:09:38 +0000

    notelistview.html() works for 0 or many items

--- spec/noteListViewSpec.js
+++ spec/noteListViewSpec.js
@@ -3,11 +3,18 @@ describe('NoteListView', function(){
   noteList = new NoteList();
   noteListView = new NoteListView(noteList);
   describe('.html()', function(){
-    it('returns html list', function(){
-      noteList.create('Ruby');
-      expect(noteListView.html()).toBe('<ul><li>Ruby</li></ul>')
-
+    it('returns html list for 0 items', function(){
+      expect(noteListView.html()).toBe('<ul></ul>');
     });
+    it('returns html list for one item', function(){
+      noteList.create('Ruby');
+      expect(noteListView.html()).toBe('<ul><li>Ruby</li></ul>');
+    });
+    it('returns html list for two items', function(){
+      noteList.create('Javascript');
+      expect(noteListView.html()).toBe('<ul><li>Ruby</li><li>Javascript</li></ul>');
+    });
+
 
   });
 

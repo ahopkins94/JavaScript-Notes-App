b304b7b02014ca52a9ba682b1ee8288cc79c9830
Anouska Hopkins <anouskahopkins@gmail.com>
2018-11-15 15:28:11 +0000

    displays note on submit

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,13 +1,22 @@
 describe("NoteController", function() {
   var noteList = new NoteList();
+  noteList.create("Favourite drink: seltzer");
   var controller = new NoteController(noteList);
 
   describe("On initialization", function() {
-    it("adds seltzer note to notelist", function() {
-      expect(noteList.notes()[0].text()).toBe("Favourite drink: seltzer");
-    });
     it("creates notelist view with notelist", function() {
       expect(controller.noteListView.noteList).toBe(noteList);
+    });
+  });
+
+  describe(".displayNewNote()", function() {
+    it("displays text of new note", function() {
+      var element = document.createElement('div')
+      element.id = 'app'
+      document.body.appendChild(element)
+      controller.displayNewNote("lemonade")
+      expect(element.innerHTML).toBe("<ul><li><a href=\"#notes/0\">Favourite drink: sel...</a></li><li><a href=\"#notes/1\">lemonade</a></li></ul>")
+      controller.noteListView.noteList._notes.pop()
     });
   });
 
@@ -40,14 +49,27 @@ describe("NoteController", function() {
     it("adds event listener to display current note on hashchange", function() {
       var callbackMock = (() => {this.displayCurrentNote();});
       var expectedArguments = ["hashchange", callbackMock].toString();
-
       var eventListenerArguments = [];
       window.addEventListener = function(event, callback) {
          eventListenerArguments = [event, callback.toString()].toString();
       };
       controller.displayNoteOnClick();
-
       expect(eventListenerArguments).toBe(expectedArguments);
     });
+  });
+
+  describe(".displayNoteOnSubmit()", function() {
+    // it("adds event listener to display new note on submit", function() {
+    //   window.preventDefault = () => {}
+    //   var submitEvent = "event"
+    //   var callbackMock = (submitEvent => {this.displayNewNote();});
+    //   var expectedArguments = ["submit", callbackMock].toString();
+    //   var eventListenerArguments = [];
+    //   window.addEventListener = function(event, callback) {
+    //      eventListenerArguments = [event, callback.toString()].toString();
+    //   };
+    //   controller.displayNoteOnSubmit();
+    //   expect(eventListenerArguments).toBe(expectedArguments);
+    // });
   });
 });
